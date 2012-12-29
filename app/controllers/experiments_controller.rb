2fe00c973252b8fa956bdb1730d8151178a73b72
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-12-30 03:00:31 +0900

    Set response time

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -5,7 +5,10 @@ class ExperimentsController < ApplicationController
 
   def update
     @experiment = Experiment.find(params[:id])
-    @experiment.update_attributes!(params[:experiment])
+    update_params = params[:experiment]
+    update_params[:accept] = true if params[:commit] == 'Accept'
+    update_params[:accept] = false if params[:commit] == 'Reject'
+    @experiment.update_attributes!(update_params)
     next_exp = Experiment.where(user_id: @experiment.user_id, accept: nil).first
     if next_exp
       redirect_to experiment_path(next_exp)
