e822eb7d62d8d1880fc2ddabb994aed6f8b4dc15
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-12-30 03:48:46 +0900

    Fix undone_experiment relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :name
   has_many :experiments, dependent: :destroy
-  has_many :undone_experiments, class_name: "Experiment", conditions: [accept: nil]
+  has_many :undone_experiments, class_name: "Experiment", conditions: {accept: nil}
 end
