e81f61ec64e4322178eb32f54f82d753fad92424
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-12-30 00:57:28 +0900

    Show initial experiment page

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,4 +1,5 @@
 class ExperimentsController < ApplicationController
   def show
+    @experiment = Experiment.find(params[:id])
   end
 end
--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -15,7 +15,7 @@ class Experiment < ActiveRecord::Base
     2.times do
       exp_list.shuffle!
       exp_list.each do |ls|
-        Experiment.create(user_id: user.id, face: ls[0] share: ls[1])
+        user.experiments.create(face: ls[0], share: ls[1])
       end
     end
   end
