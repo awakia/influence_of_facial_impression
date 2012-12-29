c3a634f26df1996137ea87e26f4f8f16af41fb9c
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-12-29 20:53:41 +0900

    Add generate_set function to experiment

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,4 +1,22 @@
 class Experiment < ActiveRecord::Base
   belongs_to :user
   attr_accessible :accept, :face, :response_time, :share
+
+  SHARES = [500, 450, 400, 350, 300, 250, 200, 150, 100]
+  FACES = 25
+
+  def self.generate_set(user)
+    exp_list = []
+    FACES.times do |face|
+      SHARES.each do |share|
+        exp_list << [face, share]
+      end
+    end
+    2.times do
+      exp_list.shuffle!
+      exp_list.each do |ls|
+        Experiment.create(user_id: user.id, face: ls[0] share: ls[1])
+      end
+    end
+  end
 end
