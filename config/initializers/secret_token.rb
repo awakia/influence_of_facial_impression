b267787a92588fc3eb7a5ba0d59fa6a2c9f2c271
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-12-29 19:07:42 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfluenceOfFacialImpression::Application.config.secret_token = 'b69cc09ea7b72c5fd7f9049315570029b5f93544a9d3f73d859d38e57da8faca77a0d4f43a3b9ef8305e98437f3927a4316c564c06e69650aa009c2d8157fa7a'
